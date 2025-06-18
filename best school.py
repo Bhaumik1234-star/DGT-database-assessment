2ddf1b78f7fdfc9444f042cd2bd1e17fce532535
Bhaumik1234-star <bhaumikrathore@riccarton.school.nz>
2025-06-18 14:50:06 +1200

    last part

---
+++ best school.py
@@ -0,0 +1,66 @@
+# Import the libraries to connect to the database and present the information in tables
+import sqlite3
+from tabulate import tabulate
+
+# This is the filename of the database to be used
+DB_NAME = 'best_schools.db'
+
+def print_query(view_name:str):
+    ''' Prints the specified view from the database in a table '''
+    # Set up the connection to the database
+    db = sqlite3.connect(DB_NAME)
+    cursor = db.cursor()
+    # Get the results from the view
+    sql = "SELECT * FROM '" + view_name + "'"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    # Get the field names to use as headings
+    field_names = "SELECT name from pragma_table_info('" + view_name + "') AS tblInfo"
+    cursor.execute(field_names)
+    headings = list(sum(cursor.fetchall(),()))
+    # Print the results in a table with the headings
+    print(tabulate(results,headings))
+    db.close()
+
+    menu_choice = ''
+    while menu_choice != 'Z':
+        menu_choice = input('Welcome to the school database\n\n'
+                           'Type the letter for the information you want:\n'
+                           'A: Show all the schools in the database\n'
+                           'B: How many schools are private\n'
+                           'C: Show all public schools\n'
+                           'D: Which schools are composite schools\n'
+                           'E: Which schools are only for boys\n'
+                           'F: Shows all schools with more than 2000 students\n'
+                           'G: Shows all the schools name and year established before 1900/n'
+                           'H: Show all girls private schools\n'
+                           'I: Shows all the schools from newest to oldest\n'
+                           'J: Show all the schools that are for girls\n'
+                           'K: Show schools with fewer than 1000 students\n'
+                           'L: Which schools are public and for boys only\n'
+                           'M: Which schools are private and have under 800 students\n'
+                           'Z: Exit\n\nType option here: ')
+        menu_choice = menu_choice.upper()
+        if menu_choice =='A':
+            print_query('Show all schools')
+        elif menu_choice == 'B':
+            print_query('How many schools')
+        elif menu_choice == 'C':
+            print_query('All public schools')
+        elif menu_choice == 'D':
+            print_query('Composite schools')
+        elif menu_choice == 'E':
+            print_query('Boys schools')
+        elif menu_choice == 'F':
+            print_query('Schools with more than 2000 studnets')
+        elif menu_choice == 'G':
+            print_query('School name year established before 1900')
+        elif menu_choice == 'H:
+            print_query('Girls private schools')
+        elif menu_choice == 'I':
+            print_query('Schools from newest to oldest')
+        elif menu_choice == 'J':
+            print_query('Schools for girl only')
+        elif menu_choice 
+
+
